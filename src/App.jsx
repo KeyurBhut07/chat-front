4f36edac112be7609d453f2750eb637dfe988ba4
Keyur Bhut <kbhut865@rku.ac.in>
2024-07-02 22:41:27 +0530

    in progress dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProttectRoutes from './components/auth/ProttectRoutes';
-import { LayoutLoaders } from './components/layout/Loaders';
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProttectRoutes from './components/auth/ProttectRoutes'
+import { LayoutLoaders } from './components/layout/Loaders'
 
-const Home = lazy(() => import('./pages/Home'));
-const Login = lazy(() => import('./pages/Login'));
-const Chat = lazy(() => import('./pages/Chat'));
-const Groups = lazy(() => import('./pages/Groups'));
-const AdminLogin = lazy(() => import('./pages/admin/AdminLogin'));
-const Dashboard = lazy(() => import('./pages/admin/Dashboard'));
+const Home = lazy(() => import('./pages/Home'))
+const Login = lazy(() => import('./pages/Login'))
+const Chat = lazy(() => import('./pages/Chat'))
+const Groups = lazy(() => import('./pages/Groups'))
+const AdminLogin = lazy(() => import('./pages/admin/AdminLogin'))
+const Dashboard = lazy(() => import('./pages/admin/Dashboard'))
+const ChatManagement = lazy(() => import('./pages/admin/ChatManagement'))
+const MessageManagement = lazy(() => import('./pages/admin/MessageManagement'))
+const UserManagement = lazy(() => import('./pages/admin/UserManagement'))
 
-let user = true;
+let user = true
 const App = () => {
   return (
     <BrowserRouter>
@@ -31,13 +34,16 @@ const App = () => {
           />
 
           <Route path="/admin" element={<AdminLogin />} />
-          <Route path="/admin/dashboard" element={<Dashboard/>}/>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/messages" element={<MessageManagement />} />
+          <Route path="/admin/user-managment" element={<UserManagement />} />
+          <Route path="/admin/group-management" element={<ChatManagement />} />
         </Routes>
       </Suspense>
        
     </BrowserRouter>
-  );
-  ƒ;
-};
+  )
+  ƒ
+}
 
-export default App;
+export default App
--- src/components/styles/StyledComponent.jsx
+++ src/components/styles/StyledComponent.jsx
@@ -1,35 +1,59 @@
-import { styled } from "@mui/material";
-import { Link as LinkCoponent } from "react-router-dom";
-import { grayColor } from "../constants/color";
+import { styled } from '@mui/material'
+import { Link as LinkCoponent } from 'react-router-dom'
+import { grayColor } from '../constants/color'
 
-export const VisuallyHiddenInput = styled("input")({
-  position: "absolute",
-  width: "1px",
-  height: "1px",
+export const VisuallyHiddenInput = styled('input')({
+  position: 'absolute',
+  width: '1px',
+  height: '1px',
   padding: 0,
   margin: -1,
-  overflow: "hidden",
-  clip: "rect(0, 0, 0, 0)",
-  whiteSpace: "nowrap",
+  overflow: 'hidden',
+  clip: 'rect(0, 0, 0, 0)',
+  whiteSpace: 'nowrap',
   border: 0,
-});
+})
 
 export const Link = styled(LinkCoponent)`
   text-decoration: none;
   color: black;
-  padding: "1rem";
+  padding: '1rem';
   &:hover {
     background-color: rgba(0, 0, 0, 0.1);
   }
-`;
+`
 
-export const InputBox = styled("input")({
-  width: "100%",
-  height: "100%",
-  border: "none",
-  outline: "0none",
-  padding: "0 3rem",
-  borderRadius: "1.5rem",
+export const InputBox = styled('input')({
+  width: '100%',
+  height: '100%',
+  border: 'none',
+  outline: '0none',
+  padding: '0 3rem',
+  borderRadius: '1.5rem',
   backgroundColor: `${grayColor}`,
-  fontSize: "1rem",
-});
+  fontSize: '1rem',
+})
+
+export const SearchField = styled('input')`
+  padding: 1rem 2rem;
+  width: 20vmax;
+  border: none;
+  outline: none;
+  border-radius: 1.5rem;
+  background-color: #f1f1f1;
+  font-size: 1.1rem;
+`
+
+export const CurveButton = styled('button')`
+  border-radius: 1.5rem;
+  padding: 1rem 2rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background-color: black;
+  color: white;
+  font-size: 1.1rem;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`
